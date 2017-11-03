b2d74b8a65656af74e7606ee34463af155a88d54
Alexandre Quivy <alexandre.quivy@epitech.eu>
2017-11-03 16:42:39 +0100

    uses Packets

--- src/Sniffer.cpp
+++ src/Sniffer.cpp
@@ -7,8 +7,10 @@
 **
 */
 #include <cstdlib>
+#include <iostream>
 
 #include "Sniffer.h"
+#include "Packet.h"
 
 void		check_proto(t_iphdr *iph)
 {
@@ -34,12 +36,14 @@ void		check_proto(t_iphdr *iph)
 
 int		main()
 {
+  Packet  *packet;
   int  		fd;
   t_sockaddr_in	saddrin;
   int		saddrinlen;
   int		recvlen;
-  char		buffer[BUF_SIZE];
+  uint8_t		buffer[BUF_SIZE];
   t_iphdr	*iph;
+  int i = 0;
 
   fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL)); //ETH_P_ALL = receive all protocol
   if (fd < 0)
@@ -52,14 +56,19 @@ int		main()
       saddrinlen = sizeof saddrin;
       recvlen = recvfrom(fd, &buffer, BUF_SIZE, 0, (struct sockaddr*)&saddrin, (socklen_t*)&saddrinlen);
       if (recvlen < 0)
-	{
-	  printf("recvfrom failed\n");
-	  return (EXIT_FAILURE);
-	}
+        {
+          printf("recvfrom failed\n");
+          return (EXIT_FAILURE);
+        }
+      packet = new Packet((uint8_t*)&buffer, recvlen);
+      i++;
       iph = (struct iphdr*)(buffer  + sizeof(struct ethhdr));
       check_proto(iph);
       saddrin.sin_addr.s_addr = iph->saddr;
-      printf("Addr: %s\nRecv: %s\n\n", inet_ntoa(saddrin.sin_addr), buffer);
+      //printf("Addr: %s\nRecv: %s\n\n", inet_ntoa(saddrin.sin_addr), buffer);
+      std::cout << "Packet n°" << i << " of size[" << packet->get_size() << "]:" << std::endl <<
+      "[" << *packet << "]" << std::endl;
+      delete packet;
     }
   if (close(fd) < 0)
     {
