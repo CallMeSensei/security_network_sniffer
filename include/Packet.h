8950570f6ff7e23df6addc0b94d33e06a060c8c8
Alexandre Quivy <alexandre.quivy@epitech.eu>
2017-11-03 16:42:06 +0100

    Packet print in hex with ostream

--- include/Packet.h
+++ include/Packet.h
@@ -2,16 +2,23 @@
 # define PACKET_H_
 
 # include <cstddef>
+# include <cstdint>
+# include <string>
+# include <vector>
 
 class Packet
 {
 protected:
-    std::size_t     _size;
-    void*           _raw_data;
+    std::vector<uint8_t>        _data;
 
 public:
-    Packet(void*, std::size_t);
+    Packet(uint8_t* data, std::size_t len);
     ~Packet();
+
+    std::size_t           get_size(void) const;
+    std::vector<uint8_t>  get_data(void) const;
 };
+
+std::ostream& operator<<(std::ostream &out, Packet packet);
 
 #endif /* !PACKET_H_ */
--- src/Packet.cpp
+++ src/Packet.cpp
@@ -1,16 +1,36 @@
-#include <cstdlib>
 #include <cstring>
+#include <iostream>
 
 #include "Packet.h"
 
-Packet::Packet(void *raw_data, std::size_t size)
+Packet::Packet(uint8_t *data, std::size_t len)
 {
-    _size = size;
-    _raw_data = std::malloc(_size);
-    std::memcpy(_raw_data, raw_data, _size);
+    _data.assign(data, data + len);
 }
 
 Packet::~Packet()
 {
-    std::free(_raw_data);
+}
+
+std::size_t           Packet::get_size(void) const
+{
+    return _data.size();
+}
+
+std::vector<uint8_t>  Packet::get_data(void) const
+{
+    return _data;
+}
+
+std::ostream& operator<<(std::ostream &out, Packet packet)
+{
+    std::vector<uint8_t>::const_iterator    it;
+
+    out << std::hex;
+    for (auto &byte : packet.get_data())
+    {
+        out << (unsigned int)byte;
+    }
+    out << std::dec;
+    return out;
 }
