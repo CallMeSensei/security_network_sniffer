75943778f7d10ed8a505a3a067fce2e8620a9fbe
Alexandre <alexandre.jallon@epitech.eu>
2017-11-16 02:24:07 +0100

    ARP working + ICMP

--- include/PacketBuilder.hh
+++ include/PacketBuilder.hh
@@ -11,7 +11,6 @@
 
 #include <arpa/inet.h>
 #include <linux/if_packet.h>
-#include <linux/ip.h>
 #include <linux/udp.h>
 #include <stdio.h>
 #include <string.h>
@@ -20,48 +19,62 @@
 #include <sys/socket.h>
 #include <net/if.h>
 #include <netinet/ether.h>
+#include <netinet/ip_icmp.h>
 
 #include <cstring>
+
+#define BUF_SIZE 2048
+#define DEFAULT_PORT 4242
+#define DEFAULT_TTL 42
+#define IP_V4_LEN 4
+
+#define ICMP_ECHOREPLY		0	/* Echo Reply*/
+#define ICMP_DEST_UNREACH	3	/* Destination Unreachable*/
+#define ICMP_SOURCE_QUENCH	4	/* Source Quench*/
+#define ICMP_REDIRECT		5	/* Redirect (change route)*/
+#define ICMP_ECHO		8	/* Echo Request*/
+#define ICMP_TIME_EXCEEDED	11	/* Time Exceeded*/
+#define ICMP_PARAMETERPROB	12	/* Parameter Problem*/
+#define ICMP_TIMESTAMP		13	/* Timestamp Request*/
+#define ICMP_TIMESTAMPREPLY	14	/* Timestamp Reply*/
+#define ICMP_INFO_REQUEST	15	/* Information Request*/
+#define ICMP_INFO_REPLY		16	/* Information Reply*/
+#define ICMP_ADDRESS		17	/* Address Mask Request*/
+#define ICMP_ADDRESSREPLY	18	/* Address Mask Reply*/
 
 typedef struct ifreq t_ifreq;
 typedef struct ether_header t_eh;
 typedef struct sockaddr_ll t_saddrll;
 typedef struct iphdr t_iphdr;
 typedef struct udphdr t_udphdr;
-typedef struct s_arphdr
-{
-  unsigned char	ar_hrd;/* format of hardware address*/
-  unsigned char	ar_pro;/* format of protocol address*/
-  unsigned char ar_hln;/* length of hardware address*/
-  unsigned char ar_pln;/* length of protocol address*/
-  unsigned char ar_op;/* ARP opcode (command)*/
-  unsigned char ar_sha[ETH_ALEN];/* sender hardware address*/
-  unsigned char ar_sip[4];/* sender IP address*/
-  unsigned char ar_tha[ETH_ALEN];/* target hardware address*/
-  unsigned char ar_tip[4];/* target IP address*/
-}t_arphdr;
+typedef struct icmphdr t_icmphdr;
+typedef struct ether_arp t_earp;
 
-#define BUF_SIZE 2048
-#define DEFAULT_PORT 4242
-#define DEFAULT_TTL 42
+#define ARP_PKT_LEN sizeof(t_eh) + sizeof(t_earp)
 
 class PacketBuilder
 {
 private:
   int		_fd;
   t_ifreq	_ipaddr;
-  t_eh		*_eh;
   t_saddrll	_saddrll;
+  t_eh		*_eh;
   t_iphdr	*_iph;
   t_udphdr	*_udph;
-  t_arphdr	*_arph;
+  t_earp	*_arph;
+  t_icmphdr	*_icmph;
   char		_buffer[BUF_SIZE];
+  char		_arpPacket[ARP_PKT_LEN];
+  bool		_isARP = false;
   int		_len;
 public:
-  PacketBuilder(int fd, char *interface, int macaddr[]);
-  void		setIPHeader(char *sip, char *dip, int protocol, int ttl=DEFAULT_TTL); //Set sip to NULL for use real ip
+  PacketBuilder(int fd, char *interface, int dmac[]);
+  void		setIPHeader(char *sip, char *dip, int protocol = IPPROTO_IP, int ttl=DEFAULT_TTL); //Set sip to NULL for use real ip
+  void		setICMPHeader(char *sip, char *dip, int ttl=DEFAULT_TTL, int type = ICMP_ECHO);
+  void		setIGMPHeader();
+  void		setTCPHeader();
   void		setUDPHeader(char *sip, char *dip, int sport=DEFAULT_PORT, int dport=DEFAULT_PORT, int ttl=DEFAULT_TTL);
-  void		setARPHeader(int smac[], int dmac[], char *sip, char *dip);
+  void		setARPHeader(int smac[], int dmac[], char *sip, char *dip, int op = ARPOP_REQUEST);
   void		setPlayload(char *p);
   void		Send();
 };
--- src/PacketBuilder.cpp
+++ src/PacketBuilder.cpp
@@ -11,7 +11,7 @@
 
 #include "PacketBuilder.hh"
 
-PacketBuilder::PacketBuilder(int fd, char *interface_name, int macaddr[])
+PacketBuilder::PacketBuilder(int fd, char *interface_name, int dmac[])
 {
   t_ifreq	interface;
   t_ifreq	mac;
@@ -30,16 +30,16 @@ PacketBuilder::PacketBuilder(int fd, char *interface_name, int macaddr[])
   if (ioctl(fd, SIOCGIFADDR, &_ipaddr) < 0)
     perror("SIOCGIFADDR");
 
-  // ETH header
-  _eh = (t_eh *) _buffer;
+  //Etherner header
   std::memset(_buffer, 0, BUF_SIZE);
+  _eh = (t_eh *) _buffer;
   _saddrll.sll_ifindex = interface.ifr_ifindex;
   _saddrll.sll_halen = ETH_ALEN;
   for (int i = 0; i < ETH_ALEN; i++)
     {
       _eh->ether_shost[i] = ((uint8_t *)&mac.ifr_hwaddr.sa_data)[i];
-      _eh->ether_dhost[i] = macaddr[i];
-      _saddrll.sll_addr[i] = macaddr[i];
+      _eh->ether_dhost[i] = dmac[i];
+      _saddrll.sll_addr[i] = dmac[i];
     }
   _eh->ether_type = htons(ETH_P_IP);
   _len += sizeof(t_eh);
@@ -64,10 +64,33 @@ void	PacketBuilder::setIPHeader(char *sip, char *dip, int protocol, int ttl)
   _len += sizeof(t_iphdr);
 }
 
+void	PacketBuilder::setICMPHeader(char *sip, char *dip, int ttl, int type)
+{
+  //ICMP Header
+  this->setIPHeader(sip, dip, IPPROTO_ICMP, ttl);
+  _icmph = (t_icmphdr *) (_buffer + sizeof (t_iphdr) + sizeof (t_eh));
+  _icmph->type = type;
+  _icmph->code = 0;
+  _icmph->un.echo.sequence = rand();
+  _icmph->un.echo.id = rand();
+  _icmph->checksum = 0;
+  _len += sizeof(t_icmphdr);
+}
+
+void	PacketBuilder::setIGMPHeader()
+{
+  //IGMP Header
+}
+
+void	PacketBuilder::setTCPHeader()
+{
+  //TCP Header
+}
+
 void	PacketBuilder::setUDPHeader(char *sip, char *dip, int sport, int dport, int ttl)
 {
   //UDP Header
-  this->setIPHeader(sip, dip, 17, ttl);
+  this->setIPHeader(sip, dip, IPPROTO_UDP, ttl);
   _udph = (t_udphdr *) (_buffer + sizeof(t_iphdr) + sizeof(t_eh));
   _udph->source = htons(sport);
   _udph->dest = htons(dport);
@@ -76,22 +99,22 @@ void	PacketBuilder::setUDPHeader(char *sip, char *dip, int sport, int dport, int
   _len += sizeof(t_udphdr);
 }
 
-void	PacketBuilder::setARPHeader(int smac[], int dmac[], char *sip, char *dip) //Doesn't work
+void	PacketBuilder::setARPHeader(int smac[], int dmac[], char *sip, char *dip, int op)
 {
-  int	saddr[4];
-  int	daddr[4];
+  int	saddr[IP_V4_LEN];
+  int	daddr[IP_V4_LEN];
   int	spoint;
   int	dpoint;
-  char	tmp[4];
+  char	tmp[IP_V4_LEN];
 
-  for (int i = 0; i < 4; i++)
+  for (int i = 0; i < IP_V4_LEN; i++)
     {
       for (spoint = 0; sip[spoint] != '.' && sip[spoint] != '\0'; spoint++);
       for (dpoint = 0; dip[dpoint] != '.' && dip[dpoint] != '\0'; dpoint++);
-      std::memset(tmp, '\0', 4);
+      std::memset(tmp, '\0', IP_V4_LEN);
       std::memcpy(tmp, sip, spoint);
       saddr[i] = atoi(tmp);
-      std::memset(tmp, '\0', 4);
+      std::memset(tmp, '\0', IP_V4_LEN);
       std::memcpy(tmp, dip, dpoint);
       daddr[i] = atoi(tmp);
       sip = &sip[spoint + 1];
@@ -99,26 +122,35 @@ void	PacketBuilder::setARPHeader(int smac[], int dmac[], char *sip, char *dip) /
     }
   printf("%d.%d.%d.%d\n", saddr[0], saddr[1], saddr[2], saddr[3]);
   printf("%d.%d.%d.%d\n", daddr[0], daddr[1], daddr[2], daddr[3]);
+  std::memset(_arpPacket, 0, ARP_PKT_LEN);
+
+  //Ethernet Header
+  _eh = (t_eh *) _buffer;
   //ARP Header
-  _saddrll.sll_family = PF_PACKET;
-  _saddrll.sll_protocol = htons(ETH_P_ARP);
-  _arph = (t_arphdr *) (_buffer + sizeof(t_iphdr) + sizeof(t_eh));
-  _arph->ar_hrd = ARPHRD_ETHER;
-  _arph->ar_pro = htons(ETH_P_IP);
-  _arph->ar_hln = ETH_ALEN;
-  _arph->ar_pln = 4;
-  _arph->ar_op = ARPOP_REQUEST;
+  _arph = (t_earp *) (_buffer + sizeof(t_eh));
   for (int i = 0; i < ETH_ALEN; i++)
     {
-      _arph->ar_sha[i] = smac[i];
-      _arph->ar_tha[i] = dmac[i];
-      if (i < 4)
+      _eh->ether_shost[i] = smac[i];
+      _eh->ether_dhost[i] = dmac[i];
+      _arph->arp_sha[i] = smac[i];
+      _arph->arp_tha[i] = dmac[i];
+      _saddrll.sll_addr[i] = dmac[i];
+      if (i < IP_V4_LEN)
 	{
-	  _arph->ar_sip[i] = saddr[i];
-	  _arph->ar_tip[i] = daddr[i];
+	  _arph->arp_spa[i] = saddr[i];
+	  _arph->arp_tpa[i] = daddr[i];
 	}
     }
-  _len += sizeof(t_arphdr);
+  _eh->ether_type = htons(ETH_P_ARP);
+
+  _saddrll.sll_family = AF_PACKET;
+  _arph = (t_earp *) (_buffer + sizeof(t_eh));
+  _arph->ea_hdr.ar_hrd = htons(ARPHRD_ETHER);
+  _arph->ea_hdr.ar_pro = htons(ETH_P_IP);
+  _arph->ea_hdr.ar_hln = ETH_ALEN;
+  _arph->ea_hdr.ar_pln = IP_V4_LEN;
+  _arph->ea_hdr.ar_op = htons(op);
+  _len = ARP_PKT_LEN;
 }
 
 void	PacketBuilder::setPlayload(char *p)
@@ -130,6 +162,12 @@ void	PacketBuilder::setPlayload(char *p)
 
 void	PacketBuilder::Send()
 {
-  if (sendto(_fd, _buffer, _len, 0, (struct sockaddr*)&_saddrll, sizeof(struct sockaddr_ll)) < 0)
+  int	sendtoResult;
+
+  if (_isARP)
+    sendtoResult = sendto(_fd, _arpPacket, _len, 0, (struct sockaddr*)&_saddrll, sizeof(t_saddrll));
+  else
+    sendtoResult = sendto(_fd, _buffer, _len, 0, (struct sockaddr*)&_saddrll, sizeof(t_saddrll));
+  if (sendtoResult < 0)
     perror("Send");
 }
